53ea923846cc4c270addbe18a1431e7824d1720d
Alfonsus Ardani <alfonsusac@gmail.com>
2024-01-21 22:35:13 +0700

    cors issue

--- src/app/like-count/route.ts
+++ src/app/like-count/route.ts
@@ -5,10 +5,22 @@ let likes = 0
 export const dynamic = "force-dynamic"
 
 export function GET() {
-  return NextResponse.json({ likes })
+  return NextResponse.json({ likes }, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  })
 }
 
 export function POST() {
   likes++
-  return NextResponse.json({ likes })
+  return NextResponse.json({ likes }, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  })
 }
